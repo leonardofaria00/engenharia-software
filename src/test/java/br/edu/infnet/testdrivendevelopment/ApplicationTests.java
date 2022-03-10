67c56cd4b5f467ba3ffe4e8376db038732e70c42
Leonardo Faria <leonardofaria00@gmail.com>
2022-03-09 22:40:16 -0300

    First commit

---
+++ src/test/java/br/edu/infnet/testdrivendevelopment/ApplicationTests.java
@@ -0,0 +1,32 @@
+package br.edu.infnet.testdrivendevelopment;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @BeforeAll
+    static void init() {
+        //TODO
+    }
+
+    @BeforeEach
+    public void setUp() {
+        //TODO
+    }
+
+    @Test
+    void contextLoads() {
+        //TODO
+    }
+
+    @AfterAll
+    static void destroy() {
+        //TODO
+    }
+
+}
