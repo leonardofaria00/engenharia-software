1e245402a8dc1037ad3d831d71d34c83d6f58994
Leonardo Faria <leonardofaria00@gmail.com>
2022-03-14 21:38:56 -0300

    Aula prática

--- src/test/java/br/edu/infnet/testdrivendevelopment/ApplicationTests.java
+++ src/test/java/br/edu/infnet/testdrivendevelopment/ApplicationTests.java
@@ -1,10 +1,11 @@
 package br.edu.infnet.testdrivendevelopment;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import br.edu.infnet.testdrivendevelopment.domain.service.Calculadora;
+import org.junit.jupiter.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 @SpringBootTest
 class ApplicationTests {
@@ -23,6 +24,26 @@ class ApplicationTests {
     void contextLoads() {
         //TODO
     }
+
+    @Test
+    @DisplayName("Converte de Celcius para Fahrenheit")
+    public void converteDeCelsilsParaFahrenheit() {
+        final Calculadora calculadora = new Calculadora();
+
+        Double valor = calculadora.convertCelciusToFahrenheit(100.0);
+
+        assertEquals(valor, Double.valueOf(212.00));
+    }
+
+    @Test
+    @DisplayName("Converte de Celcius para Fahrenheit com Nulo")
+    public void converteDeCelciusParaFahrenheitComNull() {
+        final Calculadora calculadora = new Calculadora();
+
+        assertThrows(NullPointerException.class, () -> calculadora.convertCelciusToFahrenheit(null));
+
+    }
+
 
     @AfterAll
     static void destroy() {
