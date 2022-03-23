3b48a96744848a2e879b2462eed44821cbcddcd9
Leonardo Faria <leonardofaria00@gmail.com>
2022-03-23 12:48:53 -0300

    Added credentials

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,12 +19,11 @@ resource "aws_instance" "dev" {
 }
 
 resource "aws_dynamodb_table" "dynamodb_table_read_target" {
-  provider       = "aws.virginia"
-  name           = "table_test"
-  billing_mode   = "PAY_PER_REQUEST"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "UserId"
+  provider     = aws.virginia
+  count        = 1
+  name         = "table_test"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "UserId"
   attribute {
     name = "UserId"
     type = "S"
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,6 @@
 provider "aws" {
   alias  = "virginia"
   region = var.aws_region
+  access_key = ""
+  secret_key = ""
 }
